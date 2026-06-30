159a047a34d4051c786f066e32230cdb05dee827
IT26101179 <IT26101179@my.sliit.lk>
2026-06-30 22:25:12 +0530

    Add files via upload

---
+++ Q1/IT26101179Lab9Q1.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class IT26101179Lab9Q1 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.print("Enter value a: ");
+        double a = scanner.nextDouble();
+        
+        System.out.print("Enter value b: ");
+        double b = scanner.nextDouble();
+        
+        System.out.print("Enter value c: ");
+        double c = scanner.nextDouble();
+        
+        
+        double determinant = Math.pow(b, 2) - (4 * a * c);
+        
+        if (determinant >= 0) {
+            double root1 = (-b + Math.sqrt(determinant)) / (2 * a);
+            double root2 = (-b - Math.sqrt(determinant)) / (2 * a);
+            
+            System.out.println("Roots are real and different:");
+            System.out.printf("Root 1: %.2f\n", root1);
+            System.out.printf("Root 2: %.2f\n", root2);
+        } else {
+            System.out.println("Roots are complex and imaginary.");
+        }
+        
+        scanner.close();
+    }
+}
